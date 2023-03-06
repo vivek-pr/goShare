cc9a4da8ee3e471c7b54acf6c25b0c7fbb152f3f
vivek pradhan <vivekpradhan.91@gmail.com>
2023-03-06 20:23:40 +0530

    First Commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("template/*")
+
+	router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+    })
+
+	router.POST("/upload", func(c *gin.Context){
+		file, header, err := c.Request.FormFile("file")
+		if err != nil{
+			c.String(http.StatusBadRequest, "Bad Request")
+			return
+		}
+		defer file.Close()
+
+		dest, err := os.Create(header.Filename)
+		if err != nil{
+			c.String(http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+		defer dest.Close()
+
+		if _, err = io.Copy(dest, file); err  != nil {
+			c.String(http.StatusInternalServerError, "Internal Server error")
+			return
+		}
+		c.String(http.StatusOK, "File Uploaded")
+
+	})
+
+	router.GET("/download/:filename", func(c *gin.Context){
+		filename := c.Param("filename")
+		if _, err := os.Stat(filename); os.IsNotExist((err)){
+			c.String(http.StatusNotFound, "File not found")
+			return
+		}
+
+		c.Header("Content-Disposition", "attachment; filename="+filename)
+		c.Header("Content-Type", "application/octet-stream")
+		c.File(filename)
+	})
+
+	router.Static("/files", "./")
+	router.Run(":8080")
+}
